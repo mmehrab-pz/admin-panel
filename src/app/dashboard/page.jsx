2994d939f96732017327b2409ab22cef373fd3a7
Mehrab <mmehrab.pk@gmail.com>
2026-02-10 17:41:31 +0330

    TotalIncomeCard1 added

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,21 +2,25 @@ import { Box, Grid } from "@mui/material";
 import React from "react";
 import TotalEarningCard from "./components/TotalEarningCard";
 import TotalOrderCard from "./components/TotalOrderCard";
-
+import TotalIncomeCard1 from "./components/TotalIncomeCard1";
 
 export default function DashboardPage() {
   return (
-    <Grid container rowSpacing={3}
+    <Grid
+      container
+      rowSpacing={3}
       sx={{
-    
-        display:'flex',
-        justifyContent: 'center'
+        display: "flex",
+        justifyContent: "center",
       }}
     >
       <Grid container columnSpacing={3} sx={{}}>
         <TotalEarningCard />
         <TotalOrderCard />
-        <TotalEarningCard />
+        <Grid container spacing={3} sx={{flexDirection:'column'}}>
+          <TotalIncomeCard1 />
+          <TotalIncomeCard1 />
+        </Grid>
       </Grid>
       <Grid container columnSpacing={3} sx={{}}>
         <TotalEarningCard />
