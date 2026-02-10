790acea2c9a46aef1cdc254ef2d22fefc13014bc
Mehrab <mmehrab.pk@gmail.com>
2026-02-11 00:54:14 +0330

    dashboard responsive update

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -38,30 +38,66 @@ export default function DashboardPage() {
     //     </Grid>
     //   </Grid>
     // </Grid>
+    // <Grid
+    //   container
+    //   spacing={3}
+    //   sx={{ maxWidth: "1200px", marginInline: "auto" }}
+    // >
+    //   <Grid size={4}>
+    //     <TotalEarningCard />
+    //   </Grid>
+    //   <Grid size={4}>
+    //     <TotalOrderCard />
+    //   </Grid>
+    //   <Grid size={4} container spacing={3} sx={{flexDirection:'column'}}>
+    //     <TotalIncomeCard1 />
+    //     <TotalIncomeCard2 />
+    //   </Grid>
+    //   <Grid size={8}>
+    //     <TotalGrowth />
+    //   </Grid>
+    //   <Grid size={4}>
+    //     <PopularStocks />
+    //   </Grid>
+    //   <Grid size={12}>
+    //     <Footer />
+    //   </Grid>
+    // </Grid>
     <Grid
-      container
-      spacing={3}
-      sx={{ maxWidth: "1200px", marginInline: "auto" }}
-    >
-      <Grid size={4}>
-        <TotalEarningCard />
-      </Grid>
-      <Grid size={4}>
-        <TotalOrderCard />
-      </Grid>
-      <Grid size={4} container spacing={3} sx={{flexDirection:'column'}}>
-        <TotalIncomeCard1 />
-        <TotalIncomeCard2 />
-      </Grid>
-      <Grid size={8}>
-        <TotalGrowth />
-      </Grid>
-      <Grid size={4}>
-        <PopularStocks />
-      </Grid>
-      <Grid size={12}>
-        <Footer />
-      </Grid>
-    </Grid>
+  container
+  spacing={3}
+  sx={{ maxWidth: "1200px", marginInline: "auto" }}
+>
+  <Grid size={{ xs: 12, sm: 6, md: 4 }}>
+    <TotalEarningCard />
+  </Grid>
+
+  <Grid size={{ xs: 12, sm: 6, md: 4 }}>
+    <TotalOrderCard />
+  </Grid>
+
+  <Grid
+    size={{ xs: 12, md: 4 }}
+    container
+    spacing={3}
+    sx={{ flexDirection: "column" }}
+  >
+    <TotalIncomeCard1 />
+    <TotalIncomeCard2 />
+  </Grid>
+
+  <Grid size={{ xs: 12, md: 8 }}>
+    <TotalGrowth />
+  </Grid>
+
+  <Grid size={{ xs: 12, md: 4 }}>
+    <PopularStocks />
+  </Grid>
+
+  <Grid size={12}>
+    <Footer />
+  </Grid>
+</Grid>
+
   );
 }
