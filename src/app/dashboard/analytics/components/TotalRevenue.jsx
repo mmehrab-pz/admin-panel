da90babcefa99bfb255607621e206c386312487b
Mehrab <mmehrab.pk@gmail.com>
2026-02-12 03:47:01 +0330

    update

---
+++ src/app/dashboard/analytics/components/TotalRevenue.jsx
@@ -0,0 +1,10 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+export default function TotalRevenue() {
+  return (
+   <Box sx={{borderRadius: "8px", bgcolor: "#212946",height:'500px'}}>
+
+   </Box>
+  )
+}
--- src/app/dashboard/analytics/page.jsx
+++ src/app/dashboard/analytics/page.jsx
@@ -2,15 +2,28 @@ import { Grid } from '@mui/material'
 import React from 'react'
 import MarketShare from './components/MarketShare'
 import KeyMetrics from './components/KeyMetrics'
+import TotalRevenue from './components/TotalRevenue'
 
 export default function analytics() {
   return (
+    // <Grid container spacing={3} sx={{ maxWidth: "1200px", marginInline: "auto" }}>
+    //   <Grid size={8}>
+    //       <MarketShare />
+    //   </Grid>
+    //   <Grid size={4}>
+    //       <KeyMetrics />
+    //   </Grid>
+    //   <Grid size={4}>
+    //     <TotalRevenue />
+    //   </Grid>
+    // </Grid>
     <Grid container spacing={3} sx={{ maxWidth: "1200px", marginInline: "auto" }}>
       <Grid size={8}>
-          <MarketShare />
+        <MarketShare />
       </Grid>
-      <Grid size={4}>
-          <KeyMetrics />
+      <Grid size={4} container spacing={3} sx={{flexDirection:'column'}}>
+        <KeyMetrics />
+        <TotalRevenue />
       </Grid>
     </Grid>
   )
